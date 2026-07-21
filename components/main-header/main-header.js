1d5b7f6499d0b816d0af9542d5a661e176fd204d
zakzaks <ariefn82@gmail.com>
2026-07-21 08:24:26 +0700

    Refactor MainHeader component: integrate background as a separate component and update styles

---
+++ components/main-header/main-header.js
@@ -0,0 +1,40 @@
+import imgUrl from "@/assets/logo.png";
+import Image from "next/image";
+import Link from "next/link";
+import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
+
+export default function MainHeader() {
+	return (
+		<>
+			<MainHeaderBackground />
+			<header className={classes.header}>
+				<Link className={classes.logo} href="/">
+					<Image
+						src={imgUrl.src}
+						alt="Logo"
+						width={80}
+						height={80}
+						loading="eager"
+						priority
+					/>
+					NextLevel Food
+				</Link>
+
+				<nav className={classes.nav}>
+					<ul>
+						<li>
+							<Link href="/meals">Daftar Menu</Link>
+						</li>
+						<li>
+							<Link href="/community">Komunitas</Link>
+						</li>
+						<li>
+							<Link href="/meals/share">Berbagi</Link>
+						</li>
+					</ul>
+				</nav>
+			</header>
+		</>
+	);
+}
