8d443783f1bf16dc0ab3414a6fe2969d754b4d67
zakzaks <ariefn82@gmail.com>
2026-08-28 07:42:58 +0700

    Fix image picker to reset picked image on file selection cancellation

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -16,6 +16,7 @@ export default function ImagePicker({ label, name }) {
 		const file = event.target.files[0];
 
 		if (!file) {
+			setPickedImage(null);
 			return;
 		}
 
@@ -42,6 +43,7 @@ export default function ImagePicker({ label, name }) {
 					accept="image/png, image/jpeg"
 					ref={imageInput}
 					onChange={handleImageChange}
+					required
 				/>
 				<button
 					className={classes.button}
